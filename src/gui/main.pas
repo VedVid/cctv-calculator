3c6a9e8a001851326a5c922efa50e77660ff29f9
Tomasz Nowakowski <toalak@gmail.com>
2023-04-30 11:05:19 +0200

    Renaming the main GUI's form from Form1 to MainForm.

--- src/gui/main.pas
+++ src/gui/main.pas
@@ -10,9 +10,9 @@ uses
 
 type
 
-  { TForm1 }
+  { TMainForm }
 
-  TForm1 = class(TForm)
+  TMainForm = class(TForm)
     CalculateButton: TButton;
     FpsGroupBox: TGroupBox;
     Label1: TLabel;
@@ -83,7 +83,7 @@ type
   end;
 
 var
-  Form1: TForm1;
+  MainForm: TMainForm;
   Resolution: String = 'QCIF';
   Quality: String = 'Low';
   Compression: String = 'MJPEG';
@@ -94,9 +94,9 @@ implementation
 
 {$R *.lfm}
 
-{ TForm1 }
+{ TMainForm }
 
-procedure TForm1.CalculateButtonClick(Sender: TObject);
+procedure TMainForm.CalculateButtonClick(Sender: TObject);
 var
   flags: String = '';
   strs: TStringList;
@@ -105,131 +105,131 @@ begin
   SysUtils.ExecuteProcess('./cameras_bandwidth_calculator.exe', flags, []);
   strs := tStringList.Create;
   strs.LoadFromFile('cameras_bandwidth.csv');
-  Form1.BitratePerCameraLabel.Caption := 'Bitrate per camera: ' + strs[0].Split(',')[0] + ' Mbps';
-  Form1.TotalBitrateLabel.Caption := 'Total bitrate: ' + strs[0].Split(',')[1] + ' Mbps';
+  MainForm.BitratePerCameraLabel.Caption := 'Bitrate per camera: ' + strs[0].Split(',')[0] + ' Mbps';
+  MainForm.TotalBitrateLabel.Caption := 'Total bitrate: ' + strs[0].Split(',')[1] + ' Mbps';
 end;
 
 
-procedure TForm1.CifRadioEnter(Sender: TObject);
+procedure TMainForm.CifRadioEnter(Sender: TObject);
 begin
   Resolution := 'CIF';
 end;
 
-procedure TForm1.QcifRadioEnter(Sender: TObject);
+procedure TMainForm.QcifRadioEnter(Sender: TObject);
 begin
   Resolution := 'QCIF';
 end;
 
-procedure TForm1.FourCifRadioEnter(Sender: TObject);
+procedure TMainForm.FourCifRadioEnter(Sender: TObject);
 begin
   Resolution := '4CIF';
 end;
 
-procedure TForm1.D1RadioEnter(Sender: TObject);
+procedure TMainForm.D1RadioEnter(Sender: TObject);
 begin
   Resolution := 'D1';
 end;
 
-procedure TForm1.Mpix12RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix12RadioEnter(Sender: TObject);
 begin
   Resolution := '"12 Mpix"';
 end;
 
-procedure TForm1.Mpix16RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix16RadioEnter(Sender: TObject);
 begin
   Resolution := '"16 Mpix"';
 end;
 
-procedure TForm1.Mpix1RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix1RadioEnter(Sender: TObject);
 begin
   Resolution := '"1 Mpix"';
 end;
 
-procedure TForm1.Mpix2RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix2RadioEnter(Sender: TObject);
 begin
   Resolution := '"2 Mpix"';
 end;
 
-procedure TForm1.Mpix3RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix3RadioEnter(Sender: TObject);
 begin
   Resolution := '"3 Mpix"';
 end;
 
-procedure TForm1.Mpix4RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix4RadioEnter(Sender: TObject);
 begin
   Resolution := '"4 Mpix"';
 end;
 
-procedure TForm1.Mpix5RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix5RadioEnter(Sender: TObject);
 begin
   Resolution := '"5 Mpix"';
 end;
 
-procedure TForm1.Mpix6RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix6RadioEnter(Sender: TObject);
 begin
   Resolution := '"6 Mpix"';
 end;
 
-procedure TForm1.Mpix8RadioEnter(Sender: TObject);
+procedure TMainForm.Mpix8RadioEnter(Sender: TObject);
 begin
   Resolution := '"8 Mpix"';
 end;
 
-procedure TForm1.LowQualityRadioEnter(Sender: TObject);
+procedure TMainForm.LowQualityRadioEnter(Sender: TObject);
 begin
   Quality := 'Low';
 end;
 
-procedure TForm1.MediumQualityRadioEnter(Sender: TObject);
+procedure TMainForm.MediumQualityRadioEnter(Sender: TObject);
 begin
   Quality := 'Medium';
 end;
 
-procedure TForm1.HighQualityRadioEnter(Sender: TObject);
+procedure TMainForm.HighQualityRadioEnter(Sender: TObject);
 begin
   Quality := 'High';
 end;
 
-procedure TForm1.MjpegRadioExit(Sender: TObject);
+procedure TMainForm.MjpegRadioExit(Sender: TObject);
 begin
   Compression := 'MJPEG';
 end;
 
-procedure TForm1.Mpeg2RadioExit(Sender: TObject);
+procedure TMainForm.Mpeg2RadioExit(Sender: TObject);
 begin
   Compression := 'MPEG-2';
 end;
 
-procedure TForm1.Mpeg4RadioExit(Sender: TObject);
+procedure TMainForm.Mpeg4RadioExit(Sender: TObject);
 begin
   Compression := 'MPEG-4';
 end;
 
-procedure TForm1.h264RadioExit(Sender: TObject);
+procedure TMainForm.h264RadioExit(Sender: TObject);
 begin
   Compression := 'h.264';
 end;
 
-procedure TForm1.h265RadioExit(Sender: TObject);
+procedure TMainForm.h265RadioExit(Sender: TObject);
 begin
   Compression := 'h.265';
 end;
 
-procedure TForm1.h265PlusRadioExit(Sender: TObject);
+procedure TMainForm.h265PlusRadioExit(Sender: TObject);
 begin
   Compression := 'h.265+';
 end;
 
-procedure TForm1.FpsTrackBarChange(Sender: TObject);
+procedure TMainForm.FpsTrackBarChange(Sender: TObject);
 begin
-  FPS := AnsiString(IntToStr(Form1.FpsTrackBar.Position));
-  Form1.FpsGroupBox.Caption := 'FPS (1-30): ' + FPS;
+  FPS := AnsiString(IntToStr(MainForm.FpsTrackBar.Position));
+  MainForm.FpsGroupBox.Caption := 'FPS (1-30): ' + FPS;
 end;
 
-procedure TForm1.NumberOfCamerasTrackBarChange(Sender: TObject);
+procedure TMainForm.NumberOfCamerasTrackBarChange(Sender: TObject);
 begin
-  NumberOfCameras := AnsiString(IntToStr(Form1.NumberOfCamerasTrackBar.Position));
-  Form1.NumberOfCamerasGroupBox.Caption := 'Number of cameras (1-15): ' + NumberOfCameras;
+  NumberOfCameras := AnsiString(IntToStr(MainForm.NumberOfCamerasTrackBar.Position));
+  MainForm.NumberOfCamerasGroupBox.Caption := 'Number of cameras (1-15): ' + NumberOfCameras;
 end;
 
 end.
