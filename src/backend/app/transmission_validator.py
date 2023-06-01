ffad906b5de1d57b203800b7af0c7eea3617b618
Tomasz Nowakowski <toalak@gmail.com>
2023-06-02 00:37:29 +0200

    More detailed info from transmission validator

--- src/backend/app/transmission_validator.py
+++ src/backend/app/transmission_validator.py
@@ -12,36 +12,55 @@ def validate(**data_dict):
     no_of_cameras = data_dict["no_of_cameras"]
     if no_of_cameras > data_dict["no_of_channels"]:
         comments.append(
-            "The number of cameras exceeds the number of available channels."
+            f"The number of cameras ({no_of_cameras}) exceeds the number of available channels ({data_dict['no_of_channels']})."
         )
     if no_of_cameras > data_dict["no_of_ce_channels"]:
         comments.append(
-            "The number of cameras exceeds the number of available CE channels."
+            f"The number of cameras ({no_of_cameras}) exceeds the number of available CE channels ({data_dict['no_of_ce_channels']})."
         )
         if data_dict["no_of_channels"] > data_dict["no_of_ce_channels"]:
-            comments.append("There are available non-CE channels.")
-            comments.append("Check which channels you can legally use at the installation site.")
+            no_of_non_ce_channels = (
+                data_dict["no_of_channels"] - data_dict["no_of_ce_channels"]
+            )
+            comments.append(
+                f"There are available non-CE channels: {no_of_non_ce_channels}."
+            )
+            comments.append(
+                "Check which channels you can legally use at the installation site."
+            )
     if no_of_cameras > max_cameras_in_installation_ports:
-        comments.append("The number of cameras exceeds the number of available ports.")
+        comments.append(
+            f"The number of cameras ({no_of_cameras}) exceeds the number of available ports ({max_cameras_in_installation_ports})."
+        )
 
     # Check if there is enough receivers for the transmitters on the site.
     if (
-            data_dict["no_of_transmitters"] > data_dict["no_of_receivers"] * data_dict["max_tx_to_rx"]
+        data_dict["no_of_transmitters"]
+        > data_dict["no_of_receivers"] * data_dict["max_tx_to_rx"]
     ):
-        comments.append("There is not enough receivers to connect all transmitters.")
+        comments.append(
+            f"There is not enough receivers ({data_dict['no_of_receivers']}) to connect all transmitters ({data_dict['no_of_transmitters']}, max {data_dict['max_tx_to_rx']} tx to 1 rx)."
+        )
 
     # Check if the distance is not too great for tx+rx to work well.
     if data_dict["max_distance"] > data_dict["max_distance_between_tx_and_rx"]:
-        comments.append("The distance between the furthest points is too large.")
+        comments.append(
+            f"The distance between the furthest points ({data_dict['max_distance']} m) is too large (max {data_dict['max_distance_between_tx_and_rx']} m)"
+        )
 
     # Check the distances and bitrate.
-    average_available_bitrate = (data_dict["max_bitrate"] - data_dict["min_bitrate"]) / (
-        (data_dict["max_distance_between_tx_and_rx"] - data_dict["min_tested_distance"]) / 100
+    average_available_bitrate = (
+        data_dict["max_bitrate"] - data_dict["min_bitrate"]
+    ) / (
+        (data_dict["max_distance_between_tx_and_rx"] - data_dict["min_tested_distance"])
+        / 100
     )
-    total_available_bitrate = data_dict["max_bitrate"] - ((data_dict["average_distance"] / 100) * average_available_bitrate)
+    total_available_bitrate = data_dict["max_bitrate"] - (
+        (data_dict["average_distance"] / 100) * average_available_bitrate
+    )
     if total_available_bitrate < data_dict["total_bitrate"]:
         comments.append(
-            "Total bitrate of cameras exceeds bitrate provided by receivers."
+            f"Total bitrate of cameras ({data_dict['total_bitrate']} Mbps) exceeds bitrate provided by receivers ({int(total_available_bitrate)} Mbps)."
         )
 
     return comments
